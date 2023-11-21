f816785a5b6d72e32696d50325cbc514711d7fe6
leandrobernal-dev <108369489+leandrobernal-dev@users.noreply.github.com>
2023-11-21 14:33:53 +0800

    added user database complete authentication

--- src/app/api/auth/[...nextauth]/options.js
+++ src/app/api/auth/[...nextauth]/options.js
@@ -1,82 +1,106 @@
-import GitHubProvider from "next-auth/providers/github";
 import GoogleProvider from "next-auth/providers/google";
+import GithubProvicer from "next-auth/providers/github";
 import CredentialsProvider from "next-auth/providers/credentials";
 
-export const authOptions = {
-    providers: [
-        GitHubProvider({
-            clientId: process.env.GITHUB_ID,
-            clientSecret: process.env.GITHUB_SECRET,
-        }),
-        GoogleProvider({
-            clientId: process.env.GOOGLE_CLIENT_ID,
-            clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-            authorization: {
-                params: {
-                    prompt: "consent",
-                    access_type: "offline",
-                    response_type: "code",
-                },
-            },
-        }),
-        CredentialsProvider({
-            name: "Credentials",
-            credentials: {
-                email: {
-                    label: "Email:",
-                    type: "text",
-                    placeholder: "Your Email",
-                },
-                password: {
-                    label: "Password",
-                    type: "password",
-                    placeholder: "*******",
-                },
-            },
-            async authorize(credentials) {
-                // TODO: CONNECT TO DATABASE
-                const user = { email: credentials.email };
-                return user;
-            },
-        }),
-    ],
-    secret: process.env.NEXTAUTH_SECRET,
-    pages: {
-        signIn: "/auth/login",
-        newUser: "/auth/register",
-    },
-    // callbacks: {
-    //     async signIn({ user, profile, account, email, credentials }) {
-    //         if (account.type === "oauth") {
-    //             // TODO: Connect to Database
-    //             return await signInWithOAuth({ account, profile });
-    //         }
-    //         return true;
-    //     },
-    //     async jwt({ token, trigger, session }) {
-    //         // console.log(token);
-    //         const user = await getUserByEmail({ email: token.email });
-    //         // console.log(user);
-    //         return token;
-    //     },
-    //     async session({ session, token }) {
-    //         return session;
-    //     },
-    // },
+import bcrypt from "bcrypt";
+
+import User from "@/models/User";
+import dbConnect from "@/db/database";
+
+export const options = {
+	providers: [
+		GithubProvicer({
+			clientId: process.env.GITHUB_ID,
+			clientSecret: process.env.GITHUB_SECRET,
+		}),
+		GoogleProvider({
+			clientId: process.env.GOOGLE_CLIENT_ID,
+			clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+			authorization: {
+				params: {
+					prompt: "consent",
+					access_type: "offline",
+					response_type: "code",
+				},
+			},
+		}),
+		CredentialsProvider({
+			name: "Credentials",
+			credentials: {
+				email: {
+					label: "Email:",
+					type: "text",
+					placeholder: "Your Email",
+				},
+				password: {
+					label: "Password",
+					type: "password",
+					placeholder: "*******",
+				},
+			},
+			async authorize(credentials) {
+				await dbConnect();
+				const user = await User.findOne({
+					email: credentials.email,
+				}).populate("urls");
+				if (!user) throw new Error("Email or Password is Incorrect!");
+
+				const compare = await bcrypt.compare(
+					credentials.password,
+					user.password,
+				);
+				if (!compare)
+					throw new Error("Email or Password is Incorrect!");
+
+				return user;
+			},
+		}),
+	],
+	secret: process.env.NEXTAUTH_SECRET,
+	pages: {
+		signIn: "/auth/login",
+		newUser: "/auth/register",
+	},
+	callbacks: {
+		async signIn({ user, profile, account, email, credentials }) {
+			// console.log({ account, profile });
+			if (account.type === "oauth") {
+				await dbConnect();
+				return await signInWithOAuth({ account, profile });
+			}
+			return true;
+		},
+		async jwt({ token, trigger, session }) {
+			// console.log(token);
+			const user = await getUserByEmail({ email: token.email });
+			// console.log(user);
+			return token;
+		},
+		async session({ session, token }) {
+			return session;
+		},
+	},
 };
 
 async function signInWithOAuth({ account, profile }) {
-    // TODO: Check if User exist
-    const user = {};
-    if (user) return true;
+	const user = await User.findOne({ email: profile.email });
+	// console.log({ user });
+	if (user) return true;
 
-    // TODO: If no account yet, create one here
-    return true;
+	const newUser = new User({
+		email: profile.email,
+		name: profile.name,
+		provider: account.provider,
+	});
+	await newUser.save();
+	// console.log({ newUser });
+	return true;
 }
 
 async function getUserByEmail({ email }) {
-    // TODO: Get user
-    const user = {};
-    if (!user) throw new Error("Email does not exist");
-    return { ...user._doc, _id: user._id.toString() };
+	const user = await User.findOne({ email: email });
+
+	if (!user) throw new Error("Email does not exist");
+
+	return { ...user._doc, _id: user._id.toString() };
 }
